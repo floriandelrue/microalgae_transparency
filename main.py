6dd2eb964937d4d4906d412ded2cb2fe0550a89a
floriandelrue <78607988+floriandelrue@users.noreply.github.com>
2026-08-17 12:21:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ data = import_weather_data_function(your_loc, "2024-07-01", "2024-07-02")
 
 #data is the data for the graph, test
 graph_data = pd.DataFrame({"Hourly temperature": data[0]})
-table_data = pd.DataFrame({"Optimal Transparency for January": data[0][1], "Optimal Transparency for April": data[0][7], "Optimal Transparency for July": data[0][10], "Optimal Transparency for October": data[0][15]})
+table_data = pd.DataFrame({"Optimal Transparency for January": [1], "Optimal Transparency for April": [7], "Optimal Transparency for July": [10], "Optimal Transparency for October": [15]})
 # Graph display
 st.line_chart(graph_data)
 
