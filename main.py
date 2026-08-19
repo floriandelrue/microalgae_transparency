596de451dd4ced27aa4f77680b57a2403fb80cb2
floriandelrue <78607988+floriandelrue@users.noreply.github.com>
2026-08-19 05:56:14 +0200

    Update main.py
    Test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from Import_Weather_Data import *
 
-# Fonction pour calculer les moyennes horaires
+# Function to calculate the hourly average
 def calculate_hourly_averages(data):
   if data is None:
     return np.zeros(24)
@@ -25,8 +25,11 @@ data2 = import_weather_data_function(your_loc, "2024-07-01", "2024-07-31")
 graph_data = pd.DataFrame({"Hourly temperature": data[0]})
 table_data = pd.DataFrame({"Optimal Transparency for January": [1], "Optimal Transparency for April": [7], "Optimal Transparency for July": [10], "Optimal Transparency for October": [15]})
 graph_data2 = pd.DataFrame({"Average Hourly temperature": calculate_hourly_averages(data2[0])})
+
 # Graph display
 st.line_chart(graph_data)
 
 # Table display
 st.write(table_data)
+
+st.line_chart(graph_data2)
