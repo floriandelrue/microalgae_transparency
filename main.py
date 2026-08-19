d1a2965ec0a8a2a2fb096533b1f1d64511ddb18c
floriandelrue <78607988+floriandelrue@users.noreply.github.com>
2026-08-19 05:53:55 +0200

    Update main.py
    Test

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pandas as pd
 import streamlit as st
 from Import_Weather_Data import *
 
+# Fonction pour calculer les moyennes horaires
+def calculate_hourly_averages(data):
+  if data is None:
+    return np.zeros(24)
+  hourly_averages = np.zeros(24)
+  for i in range(24):
+    hourly_averages[i] = np.mean(data[i::24])
+  return hourly_averages
 # Title
 st.title("Microalgae Transparency Model, v0.01")
 
@@ -9,11 +17,14 @@ st.title("Microalgae Transparency Model, v0.01")
 your_loc = st.text_input("Which city do you want the microalgae culture located? ")
 
 # Test the weather data function
-data = import_weather_data_function(your_loc, "2024-07-01", "2024-07-02")
+data = import_weather_data_function(your_loc, "2024-07-01", "2024-07-01")
+data2 = import_weather_data_function(your_loc, "2024-07-01", "2024-07-31")
+
 
 #data is the data for the graph, test
 graph_data = pd.DataFrame({"Hourly temperature": data[0]})
 table_data = pd.DataFrame({"Optimal Transparency for January": [1], "Optimal Transparency for April": [7], "Optimal Transparency for July": [10], "Optimal Transparency for October": [15]})
+graph_data2 = pd.DataFrame({"Average Hourly temperature": calculate_hourly_averages(data2[0])})
 # Graph display
 st.line_chart(graph_data)
 
