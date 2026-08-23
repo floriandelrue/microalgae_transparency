542b4b3d0326836b81483a4b369ca35c56ca38fb
floriandelrue <78607988+floriandelrue@users.noreply.github.com>
2026-08-23 17:04:32 +0200

    Update Culture_Temperature.py

--- Culture_Temperature.py
+++ Culture_Temperature.py
@@ -6,8 +6,7 @@
 
 import numpy as np
 
-global depth, culture_absorptivity, nb_layer, C_p, rho, sigma, \
-        e_w, A_evap, B_evap, A_conv, B_conv
+
 
 def Culture_Temperature_function(dt, nb_hours, Temperature_Control, T_limit, raceway_area, depth, hourly_global_radiation, hourly_relative_humidity_2m, hourly_temperature_2m, hourly_dew_point_2m, hourly_wind_speed_10m):
     global depth, culture_absorptivity, nb_layer, C_p, rho, sigma, \
