2fbd7ebb887d3fe3a18e410040b83779e9e0f41b
floriandelrue <78607988+floriandelrue@users.noreply.github.com>
2026-08-29 06:46:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -243,9 +243,12 @@ section[data-testid="stSidebar"] div[data-testid="stElementContainer"]:has(.kate
 }
 /* Increase the space between the lines for the second part of the sidebar */
 .second-part {
-    line-height: 4 !important;
-    margin-top: 40px !important;
-    margin-bottom: 40px !important;
+    line-height: 2 !important;
+    margin-top: 20px !important;
+    margin-bottom: 20px !important;
+    display: flex;
+    flex-direction: column;
+    gap: 20px !important;
 }
 </style>
 """, unsafe_allow_html=True)
